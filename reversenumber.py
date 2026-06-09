aacb6361bdb635ab99ae0922e13afd3e1532a124
Debmalya <db0668191@gmail.com>
2026-06-09 22:40:54 +0530

    This is number reversing

---
+++ reversenumber.py
@@ -0,0 +1,6 @@
+n=int(input("Enter any mubers="))
+num=n
+while num > 0:
+    last_digit=num%10
+    print(last_digit)
+    num=num//10
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+# n=1874
+# last_num=n%10 # this gives me the last digit
+# num=n//10 # this gives me the except last digit 
+# print(num)
