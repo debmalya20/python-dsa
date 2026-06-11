3a85ab5c04f75224f2939c05b7d249d379a6ccc1
Debmalya <db0668191@gmail.com>
2026-06-11 23:51:54 +0530

    This is palindrom

--- armstrongNumber.py
+++ armstrongNumber.py
@@ -1,12 +1,12 @@
-n=int(input("Enter the number="))
-temp=n
-length=len(str(n))
+n=int(input("Enter the number="))# this is taking the input 
+temp=n # we are storing the origns=al num in to an veriable
+length=len(str(n)) # we are taking out the length of the input
 
 total=0
-while n>0:
-    last_digit=n%10
-    total=total+(last_digit**length)
-    n=n//10
+while n>0: # this goes till the number is greater than zero
+    last_digit=n%10 # we are taking out the last digit
+    total=total+(last_digit**length) # we are multipying the power of the last digit
+    n=n//10 # and removing the last digit because we have done the execution 
 if temp==total:
     print(f"Your armstomr number is{total}")
 else:
