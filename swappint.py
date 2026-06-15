3accd3098ddefd2e3456b12a5a3eab6d2dbf9bc8
Debmalya <db0668191@gmail.com>
2026-06-15 17:25:10 +0530

    This is swapping numbers

---
+++ swappint.py
@@ -0,0 +1,22 @@
+# a=20
+# b=10
+
+# print(f"Before swapping a={a} and b={b}\n")
+
+# #1st method 
+
+# a=a+b #a= 20+10=30
+# b=a-b #b= 30-10=20
+# a=a-b #a= 30-20=10
+
+# print(f"After swapping the value of a={a} and b={b}\n")
+
+
+# #2nd method
+# a=input("Enter a charecter=")
+# b=input("Enter b charecter=")
+
+# a,b=b,a
+# print(f"After swapping the value of a={a} and b={b}\n")
+
+
