f714631be5edc30932f146646a16452161799f07
Debmalya <db0668191@gmail.com>
2026-06-22 01:04:36 +0530

    Reverse array of selected part

--- reverseARRAY.py
+++ reverseARRAY.py
@@ -1,10 +1,22 @@
-nums=[1,2,7,6,4,3,5,9,8,10]
-l=0
-r=len(nums)-1
+# nums=[1,2,7,6,4,3,5,9,8,10]
+# l=0
+# r=len(nums)-1
 
-while l<r:
-    nums[l],nums[r]=nums[r],nums[l]
-    l +=1
-    r -=1
+# while l<r:
+#     nums[l],nums[r]=nums[r],nums[l]
+#     l +=1
+#     r -=1
 
-print(nums)
+# print(nums)
+
+# using recurtion
+
+def reverse(arr,left, right):
+    if  left >=right:
+        return
+    arr [left],arr [right]=arr [right],arr [left]
+    reverse(arr, left+1, right-1)
+
+arr=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+reverse(arr,3,7)
+print(arr)
